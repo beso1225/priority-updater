5520689c4247ec93a06de0d6ae44fed48e4e52ee
beso1225 <yutaroformisc@gmail.com>
2026-05-15 01:03:40 +0900

    feat: add flake.nix and gitignore

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Basic template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    let
+      supportSystems = with flake-utils.lib.system; [
+        x86_64-linux
+        aarch64-darwin
+      ];
+    in
+    flake-utils.lib.eachSystem supportSystems (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfreePredicate =
+            pkg:
+            builtins.elem (pkgs.lib.getName pkg) [
+            ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            swift
+          ];
+        };
+      }
+    );
+}
