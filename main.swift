768f544680e69106c90928556f2408a8784419bf
beso1225 <yutaroformisc@gmail.com>
2026-05-15 01:25:49 +0900

    feat: show the remainders

--- main.swift
+++ main.swift
@@ -11,6 +11,29 @@ store.requestFullAccessToReminders { granted, error in
         semaphore.signal()
         return
     }
+    
+    let calendars = store.calendars(for: .reminder)
+    
+    let predicate = store.predicateForIncompleteReminders(
+        withDueDateStarting: nil,
+        ending: nil,
+        calendars: calendars
+    )
+
+    store.fetchReminders(matching: predicate) { reminders in
+        guard let reminders else {
+            semaphore.signal()
+            return
+        }
+
+        for reminder in reminders {
+            if reminder.calendar.title != "課題" {
+                continue
+            }
+            print("There is a reminder with title: \(reminder.title)")
+        }
+        semaphore.signal()
+    }
 }
 
 semaphore.wait()
