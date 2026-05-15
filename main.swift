2033ea74292467f9abaa641da2582e7c46e7db00
beso1225 <yutaroformisc@gmail.com>
2026-05-15 09:07:37 +0900

    feat: enable to change priority by the date

--- main.swift
+++ main.swift
@@ -11,7 +11,6 @@ store.requestFullAccessToReminders { granted, error in
         semaphore.signal()
         return
     }
-    
     let calendars = store.calendars(for: .reminder)
     
     let predicate = store.predicateForIncompleteReminders(
@@ -26,28 +25,45 @@ store.requestFullAccessToReminders { granted, error in
             return
         }
         let now = Date()
-        let calender = Calendar.current
+        let calendar = Calendar.current
 
         for reminder in reminders {
-            if reminder.calendar.title != "課題" {
-                continue
+            guard let dueComponents = reminder.dueDateComponents,
+                let dueDate = calendar.date(from: dueComponents) else {
+                    continue
             }
-            guard let title = reminder.title else {
-                continue
+            print("Processing reminder: \(reminder.title ?? "No Title") with due date: \(dueDate)")
+            let days = calendar.dateComponents([.day], from: now, to: dueDate).day ?? Int.max
+
+            var newPriority: Int?
+            if days <= 3 && days >= 0 {
+                newPriority = 1
+            } else if days <= 7 && days > 3 {
+                newPriority = 5
+            } else if days <= 14 && days > 7 {
+                newPriority = 9 
+            } else {
+                newPriority = 0
             }
-            print("There is a reminder with title: \(title)")
-            let priority = reminder.priority
-            print("Priority: \(priority)")
-            reminder.priority = 1
-            do {
-                try store.save(reminder, commit: true)
-                print("Updated priority to 1")
-            } catch {
-                print("Failed to update reminder: \(error)")
+
+            if let newPriority = newPriority, reminder.priority != newPriority {
+                reminder.priority = newPriority
+                do {
+                    try store.save(reminder, commit: true)
+                    print("Updated reminder: \(reminder.title ?? "No Title") with new priority: \(newPriority)")
+                } catch {
+                    print("Failed to update reminder: \(error)")
+                }
             }
+        }
+        do {
+            try store.commit()
+        } catch {
+            print("Failed to commit changes: \(error)")
         }
         semaphore.signal()
     }
 }
 
 semaphore.wait()
+
